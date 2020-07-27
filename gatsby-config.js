4faa6cf227d7bffed631c5e5a8a039d0dc36f7b6
ogugu <e.cos.isin@gmail.com>
2020-07-27 09:41:00 +0900

    chore: katexプラグインの導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -204,5 +204,19 @@ module.exports = {
     },
     'gatsby-plugin-flow',
     'gatsby-plugin-optimize-svgs',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          }
+        ],
+      },
+    },
   ]
 };
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout';
 import Post from '../components/Post';
 import { useSiteMetadata } from '../hooks';
 import type { MarkdownRemark } from '../types';
+import "katex/dist/katex.min.css"
 
 type Props = {
   data: {
